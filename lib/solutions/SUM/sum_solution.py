b0036e449e2e7b9066b55c8de747a5434fa06691
sbx_user1051 <sbx_user1051@169.254.50.115>
2025-11-15 22:05:06 +0000

    Sat Nov 15 22:05:06 UTC 2025

--- lib/solutions/SUM/sum_solution.py
+++ lib/solutions/SUM/sum_solution.py
@@ -5,10 +5,11 @@ class SumSolution:
         if not isinstance(x, int) or isinstance(y, int):
             raise TypeError("input must be integers")
         
-        if (0 < x < 100) or (0 < y < 100):
-            raise ValueError("input must be positive integers between 0-100")
+        if (0 <= x <= 100) or (0 <= y <= 100):
+            raise ValueError("input must be positive integers between 0-100 inclusive")
 
         return x+y
+
 
 
 
