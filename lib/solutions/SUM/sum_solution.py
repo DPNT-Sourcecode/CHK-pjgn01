d9b4ccb8883c3941ef9ba70ab50418f38ac9fe58
sbx_user1051 <sbx_user1051@169.254.50.115>
2025-11-15 21:55:44 +0000

    Sat Nov 15 21:55:44 UTC 2025

--- lib/solutions/SUM/sum_solution.py
+++ lib/solutions/SUM/sum_solution.py
@@ -2,7 +2,9 @@
 class SumSolution:
     
     def compute(self, x, y):
+        if not isinstance(x, int) || isinstance(y, int):
+            raise TypeError("")
 
-        
         raise NotImplementedError()
+
 
