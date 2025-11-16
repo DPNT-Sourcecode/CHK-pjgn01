2d5b9826d8c03c418b9723ad365aa5f87288dfbd
sbx_user1051 <sbx_user1051@169.254.2.211>
2025-11-16 18:14:11 +0000

    Sun Nov 16 18:14:11 UTC 2025

--- lib/solutions/HLO/hello_solution.py
+++ lib/solutions/HLO/hello_solution.py
@@ -12,6 +12,3 @@ class HelloSolution:
         return hello
 
         
-
-
-
--- lib/solutions/SUM/sum_solution.py
+++ lib/solutions/SUM/sum_solution.py
@@ -5,7 +5,7 @@ class SumSolution:
         if not isinstance(x, int) or not isinstance(y, int):
             raise TypeError("input must be integers")
         
-        if (0 <= x <= 100) or (0 <= y <= 100):
+        if not (0 <= x <= 100) or not (0 <= y <= 100):
             raise ValueError("input must be positive integers between 0-100 inclusive")
 
         return x+y
