8ff1c92adf8575111818ef10ee1be5ddf1c4b2db
sbx_user1051 <sbx_user1051@169.254.2.211>
2025-11-16 21:44:34 +0000

    Sun Nov 16 21:44:34 UTC 2025

--- test/solution_tests/CHK/test_checkout_solution.py
+++ test/solution_tests/CHK/test_checkout_solution.py
@@ -65,6 +65,26 @@ class TestCheckout():
             ["D", 15],
             ["E", 40],
             ["F", 10],
+            ["G", 20],
+            ["H", 10],
+            ["I", 35],
+            ["J", 60],
+            ["K", 80],
+            ["L", 90],
+            ["M", 15],
+            ["N", 40],
+            ["O", 10],
+            ["P", 50],
+            ["Q", 30],
+            ["R", 50],
+            ["S", 30],
+            ["T", 20],
+            ["U", 40],
+            ["V", 50],
+            ["W", 20],
+            ["X", 90],
+            ["Y", 10],
+            ["Z", 50],
             ["AAAAAAAAAA", 400],
             ["EE", 80],
             ["EEB", 80],
@@ -77,3 +97,4 @@ class TestCheckout():
 
         for basket, expected in test_cases:
             assert CheckoutSolution().checkout(basket) == expected
+
