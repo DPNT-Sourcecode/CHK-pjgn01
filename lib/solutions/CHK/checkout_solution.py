8980ab6a75591de3b73bfd9e621ac31946273947
sbx_user1051 <sbx_user1051@169.254.2.211>
2025-11-16 18:20:26 +0000

    Sun Nov 16 18:20:26 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,8 +1,11 @@
 
+
+
 # A supermarket checkout that calculates the total price of a number of items
 class CheckoutSolution:
 
     # skus = unicode string
     def checkout(self, skus):
-        raise NotImplementedError()
+        
+
 
