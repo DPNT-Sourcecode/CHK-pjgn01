ad7ae5f0ba490fb11c96206307f935f852963fa2
sbx_user1051 <sbx_user1051@169.254.2.211>
2025-11-16 19:18:19 +0000

    Sun Nov 16 19:18:19 UTC 2025

--- test/solution_tests/CHK/test_checkout_solution.py
+++ test/solution_tests/CHK/test_checkout_solution.py
@@ -4,29 +4,26 @@ class TestCheckout():
     def test_empty_basket(self):
         assert CheckoutSolution().checkout("") == 0
 
-    def test_empty_basket2(self):
-        assert CheckoutSolution().checkout("") == 0
-
     def test_invalid_item(self):
         assert CheckoutSolution().checkout('Z') == -1
 
     def test_one_of_each(self):
-        basket = ['ABCD']
+        basket = 'ABCD'
         assert CheckoutSolution().checkout(basket) == 115
 
     def test_A_offer_once(self):
-        basket = ['AAAA']
+        basket = 'AAAA'
         # 3A for 130 + 50
         assert CheckoutSolution().checkout(basket) == 180
 
     def test_B_offer_twice(self):
-        basket = ['BBB']
+        basket = 'BBBB'
         # (2B for 45) * 2
         assert CheckoutSolution().checkout(basket) == 90
 
     def test_3_of_everything(self):
-        basket = ['AAA'] # 3A for 130
-        basket.extend(['BBB']) # 2B for 45+30 = 75
-        basket.extend(['CCC']) # 20 * 3 = 60
-        basket.extend(['DDD']) # 15 * 3 = 45
+        basket = 'AAA' # 3A for 130
+        basket = basket + 'BBB' # 2B for 45+30 = 75
+        basket = basket + 'CCC' # 20 * 3 = 60
+        basket = basket + 'DDD' # 15 * 3 = 45
         assert CheckoutSolution().checkout(basket) == 310
