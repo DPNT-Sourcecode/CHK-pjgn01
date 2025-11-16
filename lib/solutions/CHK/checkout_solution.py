dfbd12c7c1abecce813d3c1ca1c7a260432fc7cd
sbx_user1051 <sbx_user1051@169.254.2.211>
2025-11-16 18:23:26 +0000

    Sun Nov 16 18:23:26 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,11 +1,22 @@
 
-
+price_table = {
+    'A': 50,
+    'B': 30,
+    'C': 20,
+    'D': 15,
+}
 
 # A supermarket checkout that calculates the total price of a number of items
 class CheckoutSolution:
 
-    # skus = unicode string
     def checkout(self, skus):
+        if len(skus) == 0:
+            return -1
         
+        for item in skus:
+            if item not in price_table:
+                return -1
+        
+
 
 
